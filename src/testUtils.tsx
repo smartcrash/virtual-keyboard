6451f4c8ffb4c2c52645a474d214001aa8e055c7
Diego Da'Silva <smartcrashomg@gmail.com>
2022-05-25 15:13:37 -0400

    Install chakra-ui

--- src/testUtils.tsx
+++ src/testUtils.tsx
@@ -1,10 +1,15 @@
+import { ChakraProvider } from "@chakra-ui/react";
 import { render, RenderOptions } from "@testing-library/react";
 import { ReactElement } from "react";
+
+const AllTheProviders = ({ children }: { children: any }) => {
+  return <ChakraProvider>{children}</ChakraProvider>;
+};
 
 const customRender = (
   ui: ReactElement,
   options?: Omit<RenderOptions, "wrapper">
-) => render(ui, { ...options });
+) => render(ui, { wrapper: AllTheProviders, ...options });
 
 export * from "@testing-library/react";
 export { customRender as render };
