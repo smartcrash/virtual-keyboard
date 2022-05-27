cc07ca742526f7d0bb782fd671229bf44d41f33e
Diego Da'Silva <smartcrashomg@gmail.com>
2022-05-27 11:53:39 -0400

    Add display text

---
+++ src/App.test.tsx
@@ -0,0 +1,48 @@
+import App from "./App";
+import { keyCodes } from "./components/keyboard/Keyboard";
+import { render, fireEvent } from "./testUtils";
+
+describe("<App/>", () => {
+  it("renders correctly", () => {
+    render(<App />);
+  });
+
+  it("should show text that you type using the virtual keyboard", async () => {
+    const { getByTestId } = render(<App />);
+
+    const container = getByTestId("textinput");
+
+    expect(container.innerHTML).toEqual("");
+
+    fireEvent.click(getByTestId(keyCodes["h"]));
+    fireEvent.click(getByTestId(keyCodes["e"]));
+    fireEvent.click(getByTestId(keyCodes["l"]));
+    fireEvent.click(getByTestId(keyCodes["l"]));
+    fireEvent.click(getByTestId(keyCodes["o"]));
+
+    expect(container.innerHTML).toEqual("hello");
+  });
+
+  it("should not show displayable key like TABs or CTRL", async () => {
+    const { getAllByTestId, getByTestId } = render(<App />);
+
+    const container = getByTestId("textinput");
+
+    expect(container.innerHTML).toEqual("");
+
+    [
+      "backspace",
+      "tab",
+      "enter",
+      "shiftright",
+      "shiftleft",
+      "controlleft",
+      "controlright",
+      "altright",
+      "altleft",
+      "capslock",
+    ].forEach((key) => fireEvent.click(getAllByTestId(keyCodes[key])[0]));
+
+    expect(container.innerHTML.trim()).toEqual("");
+  });
+});
--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,44 @@
-import { HStack } from "@chakra-ui/react";
+import { Box, Text } from "@chakra-ui/react";
+import { useState } from "react";
 import Keyboard from "./components/keyboard";
 
 function App() {
+  const [text, setText] = useState("");
+
+  const onKeyDown = (key: string) => {
+    if (key.includes("shift")) return;
+    if (key.includes("control")) return;
+    if (key.includes("alt")) return;
+    if (key === "capslock") return;
+
+    setText((value) => {
+      switch (key) {
+        case "backspace":
+          return value.slice(0, -1);
+        case "enter":
+          return (value += "\n");
+        case "tab":
+          return (value += "\t");
+        default:
+          return (value += key);
+      }
+    });
+  };
+
   return (
-    <HStack justifyContent={"center"} alignItems={"center"} pt={20}>
-      <Keyboard />
-    </HStack>
+    <Box maxWidth={"1034px"} mx={"auto"} pt={20}>
+      <Text
+        fontSize={"5xl"}
+        fontFamily={"monospace"}
+        fontWeight={"bold"}
+        data-testid={"textinput"}
+        whiteSpace={"pre-wrap"}
+        mb={10}
+      >
+        {text}
+      </Text>
+      <Keyboard onKeyDown={onKeyDown} />
+    </Box>
   );
 }
 
